f0f3849fdb8ddb06577750339df89a5801f4257d
afifurrohman <jack4our125@gmail.com>
2026-04-21 09:28:53 +0700

    Add files via upload

---
+++ penjumlahan 2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+	
+	int main(){   
+	
+	int a;
+	int b;
+	int hasil;
+	cout<<"masukkan nilai a = ";
+	cin>>a;
+	cout<<"masukkan nilai b = ";
+	cin>>b;
+	hasil= a+b;
+	cout<<"hasil penjumlahan variabel a dan b adalah = "<<hasil;
+	
+	}
