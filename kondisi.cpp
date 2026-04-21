4b934c368fb5ab41ad10631d5e9e16eac6961e8a
afifurrohman <jack4our125@gmail.com>
2026-04-21 09:33:55 +0700

    Add files via upload

---
+++ kondisi.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	/*
+	seleksi
+-if (kondisi){
+	hasil
+	}
+-if (kondisi){
+	hasil
+	}
+	else{
+	    hasil
+	}
+-if(kondisi){
+       hasil}
+    else if(kondisi}{
+	   hasil
+	   }
+	
+	*/
+	int nilai;
+	cout <<"masukan niali = ";
+	cin >> nilai;
+	if(nilai>=80){
+		cout<<"baik sekali";
+	}else if(nilai>=70){
+	 cout<<"baik";
+	}else{
+		cout<<"kurang baik";
+	}
+	 
+	
+}
