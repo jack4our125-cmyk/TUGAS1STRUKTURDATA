9ca3e870a545a4a3f93886a3639c7a4b95154bfb
afifurrohman <jack4our125@gmail.com>
2026-04-21 09:32:42 +0700

    Add files via upload

---
+++ pengenalan 2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+//"tipe data int,float,char,string,bool"//
+int a=8;
+char b='a';
+string nama = "andi";
+float c =1.2;
+bool d=0;
+cout <<"tampikan variabel a : "<<a<<endl;
+	cout <<"tampikan variabel b : "<<b<<endl;
+	cout <<"tampikan variabel nama : "<<nama<<endl;
+	cout <<"tampikan variabel c: "<<c<<endl;
+	cout <<"tampikan variabel d : "<<a<<endl;
+	
+	
+	
+}
